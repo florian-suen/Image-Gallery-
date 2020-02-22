bce8c1dc97b49f41c03d0e63539d5f51e3ce918f
Sun Jiefeng <dipars@gmail.com>
2020-02-22 06:15:14 +0000

    basic gallery lightbox achieved

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import images from './image.index';
 import './css/index.scss';
 
-const LIGHTBOX = document.querySelector('.lightbox');
+const QUERY = {
+  lightbox: document.querySelector('.lightbox'),
+  gallery: document.querySelector('.gallery'),
+  lightboxImage: {
+    current: document.querySelector('.lightbox__image1'),
+    prev: document.querySelector('.lightbox__image0'),
+    next: document.querySelector('.lightbox__image2')
+  }
+};
+
+const STATE = {
+  imageMouseDown: false
+};
 
 const NUMBER_GENERATOR = function numberGenerator(maxNumber) {
   return Math.floor(Math.random() * maxNumber) + 1;
 };
-
-document.querySelector('.gallery').addEventListener('click', (event) => {
-  if (event.target.classList.contains('gallery__item')) {
-    const IMAGE_SOURCE = event.target.firstChild.src;
-    const LIGHTBOX_IMAGE = document.querySelector('.lightbox__image');
-    LIGHTBOX_IMAGE.src = IMAGE_SOURCE;
-    LIGHTBOX.classList.add('lightbox--open');
-  }
-});
-
-LIGHTBOX.addEventListener('click', (event) => {
-  event.currentTarget.classList.remove('lightbox--open');
-});
 
 function createImages(limit) {
   const IMAGE_ARRAY = [];
@@ -41,7 +40,6 @@ function createImages(limit) {
 
 function loadImages(limit = 20) {
   const IMAGE_ARRAY = createImages(limit);
-  const GALLERY_ELEMENT = document.querySelector('.gallery');
   const DOCUMENT_FRAGMENT = document.createDocumentFragment();
 
   IMAGE_ARRAY.forEach((element) => {
@@ -50,7 +48,73 @@ function loadImages(limit = 20) {
     NEW_DIV.appendChild(element);
     DOCUMENT_FRAGMENT.appendChild(NEW_DIV);
   });
-  GALLERY_ELEMENT.appendChild(DOCUMENT_FRAGMENT);
+  QUERY.gallery.appendChild(DOCUMENT_FRAGMENT);
 }
 
 loadImages();
+
+const INITIAL_LIGHTBOX_IMAGE_OFFSET = {
+  top: null,
+  left: null
+};
+
+function setLightboxImagePosition() {
+  INITIAL_LIGHTBOX_IMAGE_OFFSET.top = QUERY.lightboxImage.current.offsetTop;
+  INITIAL_LIGHTBOX_IMAGE_OFFSET.left = QUERY.lightboxImage.current.offsetLeft;
+  QUERY.lightboxImage.current.style.top = `${INITIAL_LIGHTBOX_IMAGE_OFFSET.top}px`;
+  QUERY.lightboxImage.current.style.left = `${INITIAL_LIGHTBOX_IMAGE_OFFSET.left}px`;
+  QUERY.lightboxImage.current.style.position = 'absolute';
+}
+
+function resetLightboxImagePosition() {
+  QUERY.lightboxImage.current.style.position = 'static';
+}
+
+const mouseMoveHandler = (event) => {
+  const MOVEMENTX = event.movementX;
+  const CURRENT_LIGHTBOX_IMAGE_OFFSET_LEFT = QUERY.lightboxImage.current.offsetLeft;
+  QUERY.lightboxImage.current.style.left = `${MOVEMENTX + CURRENT_LIGHTBOX_IMAGE_OFFSET_LEFT}px`;
+  if (CURRENT_LIGHTBOX_IMAGE_OFFSET_LEFT < 100) {
+    console.log(CURRENT_LIGHTBOX_IMAGE_OFFSET_LEFT);
+  }
+};
+
+function addMouseMoveListener() {
+  QUERY.lightbox.addEventListener('mousemove', mouseMoveHandler);
+}
+
+QUERY.gallery.addEventListener('click', (event) => {
+  if (event.target.classList.contains('gallery__item')) {
+    const IMAGE_SOURCE = event.target.firstChild.src;
+    const IMAGE_SOURCE_PREV = event.target.previousElementSibling.firstChild.src;
+    const IMAGE_SOURCE_NEXT = event.target.nextElementSibling.firstChild.src;
+    QUERY.lightboxImage.current.src = IMAGE_SOURCE;
+    QUERY.lightboxImage.prev.src = IMAGE_SOURCE_PREV;
+    QUERY.lightboxImage.next.src = IMAGE_SOURCE_NEXT;
+    QUERY.lightbox.classList.add('lightbox--open');
+  }
+});
+
+QUERY.lightbox.addEventListener('click', (event) => {
+  if (STATE.imageMouseDown) {
+    event.currentTarget.firstChild.classList.remove('lightbox__image--scale-animation');
+    STATE.imageMouseDown = false;
+    QUERY.lightbox.removeEventListener('mousemove', mouseMoveHandler);
+    resetLightboxImagePosition();
+  } else if (event.target.classList.contains('lightbox')) {
+    event.target.classList.remove('lightbox--open');
+  }
+});
+
+QUERY.lightboxImage.current.addEventListener('mousedown', (event) => {
+  event.currentTarget.classList.add('lightbox__image--scale-animation');
+  STATE.imageMouseDown = true;
+  if (QUERY.lightboxImage.current.style.position !== 'absolute') {
+    setLightboxImagePosition();
+  }
+  addMouseMoveListener();
+});
+
+QUERY.lightboxImage.current.addEventListener('dragstart', (event) => {
+  event.preventDefault();
+});
